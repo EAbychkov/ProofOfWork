092f1e0590b659efbfd7feebc496482dc385aa3a
evgeniy <bychkovea@gmail.com>
2018-01-28 16:15:20 +0700

    Base implementation

---
+++ ProofOfWork/ChallengeResult.cs
@@ -0,0 +1,10 @@
+﻿namespace ProofOfWork
+{
+    public class ChallengeResult
+    {
+        public string SolveString { get; set; }
+        public double SolveSeconds { get; set; }
+        public string SolveHash { get; set; }
+        public int NumberOfIterations { get; set; }
+    }
+}
--- ProofOfWork/Program.cs
+++ ProofOfWork/Program.cs
@@ -1,15 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ProofOfWork
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int CurrentTime => (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+
+        static void Main()
         {
+            // case number one (difficulty = 2)
+            int difficulty = 2;
+            int numberOfSolutionsFound = 0;
+            int finishTime = CurrentTime + 15;
+            do
+            {
+                ChallengeResult result = SolveTheChallenge(RandomStringGenerator.GenerateByLen(10), difficulty);
+                numberOfSolutionsFound++;
+            } while (CurrentTime < finishTime);
+            Console.WriteLine($"Total number of solutions found for (difficulty = 2): {numberOfSolutionsFound}");
+
+            // case number two (difficulty = 3)
+            difficulty = 3;
+            numberOfSolutionsFound = 0;
+            finishTime = CurrentTime + 15;
+            do
+            {
+                ChallengeResult result = SolveTheChallenge(RandomStringGenerator.GenerateByLen(10), difficulty);
+                numberOfSolutionsFound++;
+            } while (CurrentTime < finishTime);
+            Console.WriteLine($"Total number of solutions found for (difficulty = 3): {numberOfSolutionsFound}");
+
+            // case number three (difficulty = 4)
+            difficulty = 4;
+            numberOfSolutionsFound = 0;
+            finishTime = CurrentTime + 15;
+            do
+            {
+                ChallengeResult result = SolveTheChallenge(RandomStringGenerator.GenerateByLen(10), difficulty);
+                numberOfSolutionsFound++;
+            } while (CurrentTime < finishTime);
+            Console.WriteLine($"Total number of solutions found for (difficulty = 4): {numberOfSolutionsFound}");
+
+            // case number four (difficulty = 5)
+            difficulty = 5;
+            numberOfSolutionsFound = 0;
+            finishTime = CurrentTime + 15;
+            do
+            {
+                ChallengeResult result = SolveTheChallenge(RandomStringGenerator.GenerateByLen(10), difficulty);
+                numberOfSolutionsFound++;
+            } while (CurrentTime < finishTime);
+            Console.WriteLine($"Total number of solutions found for (difficulty = 5): {numberOfSolutionsFound}");
+
+            Console.ReadKey();
+        }
+
+        private static ChallengeResult SolveTheChallenge(string challengeString, int difficulty)
+        {
+            var challengeResult  = new ChallengeResult();
+            string expectedResult = new string('0', difficulty);  // if the difficulty = 4 the expectedResult will be = '0000'
+            var stopwatch = new Stopwatch();
+            stopwatch.Start(); // doing time measurement
+            do
+            {
+                challengeResult.NumberOfIterations++; // counting the number of iterations
+                challengeResult.SolveString = challengeString + RandomStringGenerator.GenerateByLen(10); // generating random string to test it
+                challengeResult.SolveHash = Sha256Generator.GenerateSha256String(challengeResult.SolveString);  // generating sha-256 from the random string
+            } while (!challengeResult.SolveHash.StartsWith(expectedResult)); // checking if we found a solution
+            challengeResult.SolveSeconds = stopwatch.Elapsed.TotalSeconds;
+            return challengeResult;
         }
     }
 }
